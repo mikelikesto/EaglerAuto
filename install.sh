c9b81aa1a2df4239e8b35c79a49deff1144357dc
mikelikesto <66426421+mikelikesto@users.noreply.github.com>
2022-08-17 19:48:43 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-gcc atcho.c -o atcho && gcc atchw.c -o atchw && gcc newo.c -o newo && gcc neww.c -o neww && gcc starto.c -o starto && gcc startw.c -o startw
+gcc atcho.c -o atcho && gcc atchw.c -o atchtw && gcc newo.c -o newo && gcc neww.c -o neww && gcc starto.c -o starto && gcc startw.c -o startw
